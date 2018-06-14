1ba412e5b350fc40ccf581392dea8e1cac664914
david19tran <david19tran@gmail.com>
2018-06-14 19:21:30 -0400

    Add files via upload

--- EditPage.php
+++ EditPage.php
@@ -14,18 +14,14 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
       $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
       break;    
     case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
+
     case "double":
       $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
       break;
     case "date":
       $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
       break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
+
   }
   return $theValue;
 }
